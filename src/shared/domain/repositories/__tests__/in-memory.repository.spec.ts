e1b137fcea79a6c9d5cddc897370f67dc446f5bb
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-03-25 12:49:36 -0300

    tests: InMemoryRepository unit tests - insert method

---
+++ src/shared/domain/repositories/__tests__/in-memory.repository.spec.ts
@@ -0,0 +1,26 @@
+import { Entity } from '../../entities/entity';
+import { InMemoryRepository } from '../in-memory.repository';
+
+type StubEntityProps = {
+  nmae: string;
+  price: number;
+};
+
+class StubEnity extends Entity<StubEntityProps> {}
+
+class StubInMemoryRepository extends InMemoryRepository<StubEnity> {}
+
+describe('InMemoryRepository uni tests', () => {
+  let sut: StubInMemoryRepository;
+
+  beforeEach(() => {
+    sut = new StubInMemoryRepository();
+  });
+
+  it('Should insert a new entity', async () => {
+    const entity = new StubEnity({ nmae: 'test name', price: 1 });
+    await sut.insert(entity);
+
+    expect(entity.toJson()).toStrictEqual(sut.items[0].toJson());
+  });
+});
