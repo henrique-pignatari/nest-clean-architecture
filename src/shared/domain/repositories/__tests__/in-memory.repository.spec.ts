9bc53e931036f3f1c1e7cdb86c20bc6c748a76e1
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-03-25 13:08:02 -0300

    tests: InMemoryRepository unit tests - update method

--- src/shared/domain/repositories/__tests__/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/in-memory.repository.spec.ts
@@ -3,7 +3,7 @@ import { NotFoundError } from '../../errors/not-found-error';
 import { InMemoryRepository } from '../in-memory.repository';
 
 type StubEntityProps = {
-  nmae: string;
+  name: string;
   price: number;
 };
 
@@ -19,20 +19,20 @@ describe('InMemoryRepository uni tests', () => {
   });
 
   it('Should insert a new entity', async () => {
-    const entity = new StubEnity({ nmae: 'test name', price: 1 });
+    const entity = new StubEnity({ name: 'test name', price: 1 });
     await sut.insert(entity);
 
     expect(sut.items[0].toJSON()).toStrictEqual(entity.toJSON());
   });
 
-  it('Should throw an erro when entity not found', async () => {
+  it('Should throw an error when entity not found', async () => {
     await expect(sut.findById('fakeId')).rejects.toThrow(
       new NotFoundError('Entity not found'),
     );
   });
 
   it('Should find an entity by id', async () => {
-    const entity = new StubEnity({ nmae: 'test name', price: 1 });
+    const entity = new StubEnity({ name: 'test name', price: 1 });
     await sut.insert(entity);
 
     const result = await sut.findById(entity.id);
@@ -41,11 +41,37 @@ describe('InMemoryRepository uni tests', () => {
   });
 
   it('Should return all entities', async () => {
-    const entity = new StubEnity({ nmae: 'test name', price: 1 });
+    const entity = new StubEnity({ name: 'test name', price: 1 });
     await sut.insert(entity);
 
     const result = await sut.findAll();
 
     expect(result).toStrictEqual([entity]);
+  });
+
+  it('Should throw an error on update when entity not found', async () => {
+    const entity = new StubEnity({ name: 'test name', price: 1 });
+
+    await expect(sut.update(entity)).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    );
+  });
+
+  it('Should update an entity', async () => {
+    const entity = new StubEnity({ name: 'test name', price: 1 });
+    await sut.insert(entity);
+
+    const entityUpdated = new StubEnity(
+      {
+        name: 'test name',
+        price: 1,
+      },
+      entity.id,
+    );
+
+    await sut.update(entityUpdated);
+
+    const result = await sut.findById(entity.id);
+    expect(result.toJSON()).toStrictEqual(entityUpdated.toJSON());
   });
 });
