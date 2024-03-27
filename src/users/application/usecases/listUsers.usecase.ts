6b9de7ec1dde10b4ba23b407dbfa672a93ae5e1e
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-03-26 21:54:37 -0300

    feat: add pagination output

--- src/users/application/usecases/listUsers.usecase.ts
+++ src/users/application/usecases/listUsers.usecase.ts
@@ -1,11 +1,16 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { UseCase as DefaultUsecase } from '@/shared/application/usecases/use-case';
 import { SearchInput } from '@/shared/application/dtos/search-input';
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/shared/application/dtos/pagination-output';
+import { UserOutput, UserOutputMapper } from '../dtos/user-output';
 
 export namespace ListUsersUseCase {
   export type Input = SearchInput;
 
-  export type Output = void;
+  export type Output = PaginationOutput<UserOutput>;
 
   export class UseCase implements DefaultUsecase<Input, Output> {
     constructor(private userRepository: UserRepository.Repository) {}
@@ -13,7 +18,14 @@ export namespace ListUsersUseCase {
     async execute(input: Input): Promise<Output> {
       const params = new UserRepository.SearchParams(input);
       const searchResult = await this.userRepository.search(params);
-      return;
+      return this.toOutput(searchResult);
+    }
+
+    private toOutput(searchResult: UserRepository.SearchResult): Output {
+      const items = searchResult.items.map(item =>
+        UserOutputMapper.toOuput(item),
+      );
+      return PaginationOutputMapper.toOutput(items, searchResult);
     }
   }
 }
