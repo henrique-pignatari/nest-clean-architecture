a080fe0af52bda095ce66a4e842279ddcfe45d01
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-04-03 01:40:52 -0300

    feat: implement useCases to userController

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -6,37 +6,84 @@ import {
   Patch,
   Param,
   Delete,
+  Inject,
+  HttpCode,
+  Query,
+  Put,
 } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { SignupDto } from './dtos/signup.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { SignupUseCase } from '../application/usecases/signup.usecase';
+import { SigninUseCase } from '../application/usecases/signin.usecase';
+import { UpdateUserUseCase } from '../application/usecases/update-user.usecase';
+import { UpdatePasswordUseCase } from '../application/usecases/update-password.usecase copy';
+import { DeleteUserUseCase } from '../application/usecases/deleteUser.usecase copy';
+import { GetUserUseCase } from '../application/usecases/getUser.usecase';
+import { ListUsersUseCase } from '../application/usecases/listUsers.usecase';
+import { SigninDto } from './dtos/signin.dto';
+import { ListUsersDto } from './dtos/list-users.dto';
+import { UpdatePasswordDto } from './dtos/update-password.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  @Inject(SignupUseCase.UseCase)
+  private readonly signupUseCase: SignupUseCase.UseCase;
+
+  @Inject(SigninUseCase.UseCase)
+  private readonly signinUseCase: SigninUseCase.UseCase;
+
+  @Inject(UpdateUserUseCase.UseCase)
+  private readonly updateUserUseCase: UpdateUserUseCase.UseCase;
+
+  @Inject(UpdatePasswordUseCase.UseCase)
+  private readonly updatePasswordUseCase: UpdatePasswordUseCase.UseCase;
+
+  @Inject(DeleteUserUseCase.UseCase)
+  private readonly deleteUserUseCase: DeleteUserUseCase.UseCase;
+
+  @Inject(GetUserUseCase.UseCase)
+  private readonly getUserUseCase: GetUserUseCase.UseCase;
+
+  @Inject(ListUsersUseCase.UseCase)
+  private readonly listUsersUseCase: ListUsersUseCase.UseCase;
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() signupDto: SignupDto) {
+    return this.signupUseCase.execute(signupDto);
+  }
+
+  @HttpCode(200)
+  @Post('login')
+  async login(@Body() signinDto: SigninDto) {
+    return this.signinUseCase.execute(signinDto);
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async search(@Query() searchParams: ListUsersDto) {
+    return this.listUsersUseCase.execute(searchParams);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return this.getUserUseCase.execute({ id });
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.updateUserUseCase.execute({ id, ...updateUserDto });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  async updatePassword(
+    @Param('id') id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ) {
+    return this.updatePasswordUseCase.execute({ id, ...updatePasswordDto });
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  async remove(@Param('id') id: string) {
+    await this.deleteUserUseCase.execute({ id });
   }
 }
