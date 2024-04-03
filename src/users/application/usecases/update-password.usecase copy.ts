ac4fc958cb2c290c3973e2406367abb66fa43bb7
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-04-02 23:12:07 -0300

    feat: add updatePasswordUseCase

---
+++ src/users/application/usecases/update-password.usecase copy.ts
@@ -0,0 +1,47 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { UserOutput, UserOutputMapper } from '../dtos/user-output';
+import { UseCase as DefaultUsecase } from '@/shared/application/usecases/use-case';
+import { BadRequestError } from '@/shared/application/errors/bad-request-error';
+import { InvalidPasswordError } from '@/shared/application/errors/invalid-password-error';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
+
+export namespace UpdatePasswordUseCase {
+  export type Input = {
+    id: string;
+    password: string;
+    oldPassword: string;
+  };
+
+  export type Output = UserOutput;
+
+  export class UseCase implements DefaultUsecase<Input, Output> {
+    constructor(
+      private userRepository: UserRepository.Repository,
+      private hashProvider: HashProvider,
+    ) {}
+
+    async execute(input: Input): Promise<Output> {
+      const entity = await this.userRepository.findById(input.id);
+
+      if (!input.password || !input.oldPassword) {
+        throw new InvalidPasswordError(
+          'Old password and newPassword are required',
+        );
+      }
+
+      const checkOldPassword = await this.hashProvider.compareHash(
+        input.oldPassword,
+        entity.password,
+      );
+
+      if (!checkOldPassword) {
+        throw new InvalidPasswordError('Old password does not match');
+      }
+
+      const hashPassword = await this.hashProvider.generateHash(input.password);
+      entity.updatePassword(hashPassword);
+      await this.userRepository.update(entity);
+      return UserOutputMapper.toOuput(entity);
+    }
+  }
+}
