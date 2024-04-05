f9fa4ce1df4d4c283bfe5bca28ba3b43fb1dfee8
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-04-05 18:31:23 -0300

    tests: add userPresenter unit tests

---
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -0,0 +1,38 @@
+import { instanceToPlain } from 'class-transformer';
+import { UserPresenter } from '../../user.presenter';
+
+describe('UsersController unit tests', () => {
+  const createdAt = new Date();
+  const props = {
+    id: 'a3adb1eb-6643-40b0-bde6-37f2904800bb',
+    name: 'testName',
+    email: 'a@a.com',
+    password: 'senha123',
+    createdAt,
+  };
+  let sut: UserPresenter;
+
+  beforeEach(() => {
+    sut = new UserPresenter(props);
+  });
+
+  describe('constructor', () => {
+    it('should reflect props values', () => {
+      expect(sut.id).toEqual(props.id);
+      expect(sut.name).toEqual(props.name);
+      expect(sut.email).toEqual(props.email);
+      expect(sut.createdAt).toEqual(props.createdAt);
+      expect(sut['password']).toBeUndefined();
+    });
+
+    it('should transform to presenter', () => {
+      const output = instanceToPlain(sut);
+      expect(output).toStrictEqual({
+        id: 'a3adb1eb-6643-40b0-bde6-37f2904800bb',
+        name: 'testName',
+        email: 'a@a.com',
+        createdAt: createdAt.toISOString(),
+      });
+    });
+  });
+});
