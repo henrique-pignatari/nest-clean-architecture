e364cf968191ac38073e61f0a25e6405b4e5e928
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-04-06 00:05:38 -0300

    feat: add user dto validation pipe

--- src/users/infrastructure/dtos/update-password.dto.ts
+++ src/users/infrastructure/dtos/update-password.dto.ts
@@ -1,8 +1,14 @@
 import { UpdatePasswordUseCase } from '@/users/application/usecases/update-password.usecase copy';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdatePasswordDto
   implements Omit<UpdatePasswordUseCase.Input, 'id'>
 {
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   oldPassword: string;
 }
