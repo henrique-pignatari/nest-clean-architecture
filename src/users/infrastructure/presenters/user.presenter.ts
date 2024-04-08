9953831c8552afb5712d3669dd8fb00878a35a70
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-04-08 07:26:39 -0300

    docs: add user presenter property docs

--- src/users/infrastructure/presenters/user.presenter.ts
+++ src/users/infrastructure/presenters/user.presenter.ts
@@ -1,12 +1,20 @@
 import { CollectionPresenter } from '@/shared/infrastructure/presenter/collection.presenter';
 import { UserOutput } from '@/users/application/dtos/user-output';
 import { ListUsersUseCase } from '@/users/application/usecases/listUsers.usecase';
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 
 export class UserPresenter {
+  @ApiProperty({ description: 'User Id' })
   id: string;
+
+  @ApiProperty({ description: 'User name' })
   name: string;
+
+  @ApiProperty({ description: 'User email' })
   email: string;
+
+  @ApiProperty({ description: 'User creation date' })
   @Transform(({ value }: { value: Date }) => value.toISOString())
   createdAt: Date;
 
