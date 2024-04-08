82c9dd39b96d5aa7fe1a57ffc2f36c081bc8e740
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-04-08 07:15:01 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,33 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { ClassSerializerInterceptor } from '@nestjs/common';
 import { applyGlobalConfig } from './global-config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const swaggetConfig = new DocumentBuilder()
+    .setTitle('NestJs Clean Arch')
+    .setDescription('NestJs with clean archteture, DDD, Automated Tests')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Inform JWT to authorize',
+      name: 'Authorization',
+      scheme: 'Bearer',
+      type: 'http',
+      in: 'Header',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggetConfig);
+  SwaggerModule.setup('api', app, document);
 
   await applyGlobalConfig(app);
   await app.listen(3000, '0.0.0.0');
